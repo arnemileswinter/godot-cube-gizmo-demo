23b2e76963fc2860fba0e21b2dc21386d10038af
arnemileswinter <arnemileswinter@users.noreply.github.com>
2023-08-20 02:54:56 +0200

    adjust the cube to be a tool script s.t. a setter is invoked via gizmo

--- addons/example_cube_gizmo/example_cube.gd
+++ addons/example_cube_gizmo/example_cube.gd
@@ -1,7 +1,13 @@
+@tool 	# must be a tool script such that inspector changes invoke 
+		# the setter which in turn updates the associated gizmos.
 extends Node3D
 class_name ExampleCube
 
-@export var extents : Vector3 = Vector3(1,1,1)
+@export var extents : Vector3 = Vector3(1,1,1) : set = _set_extents
+
+func _set_extents(new):
+	extents = new
+	update_gizmos()
 
 func _init():
 	set_disable_scale(true)
